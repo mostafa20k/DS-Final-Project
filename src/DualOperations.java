06202165ca824a10f255ab9a63119918de9db4eb
Mostafa Karimpoor <mosi.reward@gmail.com>
2022-01-23 19:34:20 +0330

    First part completed

--- src/DualOperations.java
+++ src/DualOperations.java
@@ -57,9 +57,61 @@ public class DualOperations {
         newRoot.weight=newRoot.left.weight;
         if (newRoot.left.right != null)
             newRoot.weight += newRoot.left.right.weight;
-        System.out.println(newRoot.weight);
         ropes.get(first-1).setRoot(newRoot);
         ropes.remove(second-1);
+    }
+
+    public void insert(int first,int position,int second){
+        split(first,position);
+        if(second==first+1){
+            second=second+1;
+        }
+        concat(first,second);
+        concat(first,first+1);
+    }
+
+    public void split(int stringNum, int position){
+        int count=-1;
+        StringBuilder first=new StringBuilder();
+        StringBuilder second=new StringBuilder();
+        Node root = ropes.get(stringNum-1).getRoot();
+        if (root == null)
+            return;
+        Stack<Node> s = new Stack<Node>();
+        Node curr = root;
+        while (curr != null || s.size() > 0) {
+            while (curr !=  null)
+            {
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop();
+
+            if(curr.data != null){
+                for (int i = 0; i < curr.data.length(); i++) {
+                    count++;
+                    if(count<=position){
+                        first.append(curr.data.charAt(i));
+                    }else {
+                        second.append(curr.data.charAt(i));
+                    }
+                }
+            }
+            curr = curr.right;
+        }
+        Rope rope = new Rope();
+        rope.newRope(String.valueOf(first));
+        Rope rope1 = new Rope();
+        rope1.newRope(String.valueOf(second));
+        ropes.remove(stringNum-1);
+        ropes.add(stringNum-1,rope);
+        ropes.add(stringNum,rope1);
+    }
+    public void delete(int stringNum, int i, int j){
+        split(stringNum,i);
+        split(stringNum+1,j-i-2);
+        ropes.remove(stringNum);
+        concat(stringNum,stringNum+1);
     }
 }
 
